848421b2f9004a026f57e56f7636a009a353cca3
ThuyNguyen99 <thuy4111909@gmail.com>
2022-09-19 20:35:58 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/routes/contact.route.js
@@ -0,0 +1,17 @@
+const e = require("express");
+const express = require("express");
+const contacts = require("../controllers/contact.controller");
+
+const router = express.Router();
+
+router.route("/")
+    .get(contacts.findAll)
+    .post(contacts.create)
+    .delete(contacts.deleteAll);
+
+router.route("/favorite")
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contacts.delete);
+
+module.exports = router;
